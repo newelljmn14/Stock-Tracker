f149e8a9508cdb8b80ee3b808dd60d8910ea96a8
James Newell <newelljmn@bellsouth.net>
2017-09-13 17:08:39 -0500

    Update client

--- StockTrackerClient/load-landing-page.js
+++ StockTrackerClient/load-landing-page.js
@@ -1,7 +1,49 @@
+var container;
+var usernameInput;
+
 $(document).ready(function(){
-    var txt1 = "<p>Text.</p>";              // Create text with HTML
-    var txt2 = $("<p></p>").text("Text.");  // Create text with jQuery
-    var txt3 = document.createElement("p");
-    txt3.innerHTML = "Text.";               // Create text with DOM
-    $("body").append(txt1, txt2, txt3);    
+    initializeContainer();
+    createUsername();
+    createPassword();
+    createRegistration();
 });
+
+var initializeContainer = function() {
+    container = $('<div></div>', {id: 'container'});
+    
+    $('body').append(container);
+}
+
+function createUsername() {
+    var usernameContainer = $('<div></div>', {id: 'usernameContainer'});
+    var usernameLabel = $('<label>username: </label>');
+    usernameInput = $('<input type="text" />', {id: 'username'});
+    $(usernameContainer).append(usernameLabel, usernameInput);
+
+    $(container).append(usernameContainer);
+}
+
+var createPassword = () => {
+    var passwordContainer = $('<div></div>', {id: 'passwordContainer'});
+    var passwordLabel = $('<label>password: </label>');
+    $(passwordContainer).append(passwordLabel, passwordInput);
+
+    var passwordInput = $("<input>")
+        .attr("type", "password")
+        .attr("id", "password")
+        .appendTo(passwordContainer);
+
+    $(container).append(passwordContainer);
+}
+
+function createRegistration() {
+    var registerButton = '<button onClick="register()" >register</button>';
+
+    $(container).append(registerButton);
+}
+
+function register() {
+    console.log($('#password').val());
+}
+
+
